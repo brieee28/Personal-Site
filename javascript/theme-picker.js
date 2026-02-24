57ec6fa5a54fad96d6c1f76b04d930c251357942
brieee28 <briewillcox@gmail.com>
2026-02-24 12:55:38 -0600

    made the theme selector look nice

--- javascript/theme-picker.js
+++ javascript/theme-picker.js
@@ -1,10 +1,22 @@
 const rootElement = document.documentElement;
 const themeSelectElement = document.getElementById("theme-select-entry-theme");
 const colorSelectElement = document.getElementById("theme-select-entry-color");
-
 themeSelectElement.addEventListener("change", () => {
     rootElement.className = themeSelectElement.value;
 });
 colorSelectElement.addEventListener("change", () => {
     rootElement.style.setProperty("--accent-color", colorSelectElement.value);
+});
+
+const themeMenuElement = document.getElementById("theme-menu");
+const themeMenuToggleElement = document.getElementById("theme-menu-toggle");
+let themeMenuVisible = false;
+themeMenuToggleElement.addEventListener("click", () => {
+    if (themeMenuVisible == false) {
+        themeMenuVisible = true;
+        themeMenuElement.style.setProperty("display", "block");
+    } else {
+        themeMenuVisible = false;
+        themeMenuElement.style.setProperty("display", "none");
+    }
 });
