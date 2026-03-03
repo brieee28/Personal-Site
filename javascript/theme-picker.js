9c91efbd47cf555f54b8f8c39a9ac8f0a78dacbd
brieee28 <briewillcox@gmail.com>
2026-03-02 23:49:49 -0600

    changed theme picker from open-on-click to open-on-mouseenter

--- javascript/theme-picker.js
+++ javascript/theme-picker.js
@@ -2,6 +2,7 @@ const rootElement = document.documentElement;
 
 const themeMenuElement = document.getElementById("theme-menu");
 const themeMenuToggleElement = document.getElementById("theme-menu-toggle");
+/*
 let themeMenuVisible = false;
 themeMenuToggleElement.addEventListener("click", () => {
     if (themeMenuVisible == false) {
@@ -11,6 +12,16 @@ themeMenuToggleElement.addEventListener("click", () => {
         themeMenuVisible = false;
         themeMenuElement.style.setProperty("display", "none");
     }
+});
+*/
+const themesDivElement = document.getElementById("themes-div");
+themesDivElement.addEventListener("mouseenter", () => {
+    themeMenuElement.style.setProperty("opacity", 1);
+    themeMenuElement.style.setProperty("visibility", "visible");
+});
+themesDivElement.addEventListener("mouseleave", () => {
+    themeMenuElement.style.setProperty("opacity", 0);
+    themeMenuElement.style.setProperty("visibility", "hidden");
 });
 
 const themeSelectElement = document.getElementById("theme-select-entry-theme");
